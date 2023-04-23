d01e232d95ac9604e51289afd9ad688c43a9bf21
saignan <saignankarrolla695@gmail.com>
2023-04-23 13:48:56 +0530

    Pop and peek logics are updated

---
+++ Stacks_Queues/Stacks_Queues/UC1_UC2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Stacks_Queues.Program;
+
+namespace Stacks_Queues
+{
+    internal class Stack1
+    {
+        public Node top;
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+        public int Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            int data = top.data;
+            top = top.next;
+            return data;
+        }
+        public int Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return top.data;
+        }
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+
+    }
+}
