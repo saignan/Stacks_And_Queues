12e8e33f72f061ea1c78bd02624cf65fae583d1a
saignan <saignankarrolla695@gmail.com>
2023-04-23 13:16:06 +0530

    UC1 logic updated

---
+++ Stacks_Queues/Stacks_Queues/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Stacks_Queues
+{
+    internal class Program
+    {
+        //creating a node of LinkedList
+        public class Node
+        {
+            public int data;
+            public Node next;
+
+            public Node(int data)
+            {
+                this.data = data;
+                this.next = null;
+            }
+        }
+        static void Main(string[] args)
+        {
+           //UseCase 1
+            Stack1 obj = new Stack1();
+            obj.Push(70);
+            obj.Push(30);
+            obj.Push(56);
+         
+            
+        }
+    }
+}
---
+++ Stacks_Queues/Stacks_Queues/UC1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Stacks_Queues.Program;
+
+namespace Stacks_Queues
+{
+    internal class Stack1
+    {
+        private Node top;
+
+        public void Push(int data)
+        {
+            Node newNode = new Node(data);
+            if (top == null)
+            {
+                top = newNode;
+            }
+            else
+            {
+                newNode.next = top;
+                top = newNode;
+            }
+        }
+    }
+}
